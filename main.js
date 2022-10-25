02a6782d1d76171e5c1f14bf01f69d20245f16c4
Aisha <aisha.saparali@nu.edu.kz>
2022-10-25 20:10:57 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const PI = 3.14; 
+let area = function (r) {
+	result= PI *r*r 
+	return result
+};
+
+let S = area(5)
+console.log(S); 
+
+let x = 3
+let y = 7
+let z = 3 
+
+if (x>y & x>z){
+	console.log("x is the greatest")
+}
+else if (y>x & y>z){
+	console.log("y is the greatest")
+}
+else {
+	console.log("z is the greatest")
+}
+
+if (y%2==0) {
+	console.log("y is an odd number")
+}
+else {
+	console.log("y is an even number")
+}
+
+
+if (x==z) {
+	console.log("x and z are equal")
+}
+else{
+	console.log("x and z are not equal")
+}
